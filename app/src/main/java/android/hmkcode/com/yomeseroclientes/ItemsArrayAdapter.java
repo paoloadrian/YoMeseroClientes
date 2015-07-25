0d6059efc2e943987c7e6039e93e2155d1d47e00
Paolo Adrian Fernandez Roman <piro_pafr@hotmail.com>
2015-07-25 12:16:53 -0400

    probando el incremento del bton

--- app/src/main/java/android/hmkcode/com/yomeseroclientes/ItemsArrayAdapter.java
+++ app/src/main/java/android/hmkcode/com/yomeseroclientes/ItemsArrayAdapter.java
@@ -3,10 +3,12 @@ package android.hmkcode.com.yomeseroclientes;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,29 +18,50 @@ import java.util.ArrayList;
 public class ItemsArrayAdapter extends ArrayAdapter<Item> {
     private final Context context;
     private ArrayList<Item> items;
+    private ArrayList<String> names = new ArrayList<>();
+    private ArrayList<String> descriptions = new ArrayList<>();
 
     public ItemsArrayAdapter(Context context, ArrayList<Item> items) {
         super(context, R.layout.item_list, items);
         this.context = context;
         this.items = items;
+        for (int i = 0; i < items.size(); i++) {
+            names.add("Nombre: "+items.get(i).item_name);
+            descriptions.add("Tipo: "+items.get(i).item_type+" - Precio: Bs. "+items.get(i).item_price);
+        }
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.item_list, parent, false);
         TextView textView = (TextView) rowView.findViewById(R.id.firstLine);
         TextView textView2 = (TextView) rowView.findViewById(R.id.secondLine);
-        ArrayList<String> names = new ArrayList<>();
-        ArrayList<String> descriptions = new ArrayList<>();
-        for (int i = 0; i < items.size(); i++) {
-            names.add("Nombre: "+items.get(i).item_name);
-            descriptions.add("Tipo: "+items.get(i).item_type+" - Precio: Bs. "+items.get(i).item_price);
-        }
         textView.setText(names.get(position));
         textView2.setText(descriptions.get(position));
+        TextView quantity = (TextView)rowView.findViewById(R.id.cant);
+        Button button = (Button) rowView.findViewById(R.id.plusbutton);
+
+        ButtonClickListener listener = new ButtonClickListener(quantity);
+        button.setOnClickListener(listener);
+
         // change the icon for Windows and iPhone
         return rowView;
+    }
+
+    private class ButtonClickListener implements View.OnClickListener {
+        private TextView textView;
+        public ButtonClickListener(TextView tv) {
+            textView = tv;
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.plusbutton) {
+                int quant = Integer.parseInt(textView.getText().toString());
+                textView.setText(Integer.toString(quant + 1));
+            }
+        }
     }
 }
